64ab472fa29a6cc4dec819b7e5422bd5c8c3715c
Abid Hassan Kiam <kiamhasan267@gmail.com>
2026-08-20 03:15:57 +0600

    Refactor code structure for improved readability and maintainability

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,48 +1,29 @@
-const cluster = require('cluster');
-import { ValidationPipe } from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Express } from 'express';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { AppModule } from './app.module';
-import * as os from 'os';
 
-// if (cluster.isPrimary) {
-//   // Use cluster.isPrimary in newer Node.js versions
-//   const numCPUs = os.cpus().length;
-//   console.log(`Primary ${process.pid} is running`);
+const server: Express = express();
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+let cachedApp: Express | undefined;
 
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`Worker ${worker.process.pid} died`);
-//     cluster.fork(); // Restart the worker when it exits
-//   });
-// } else {
-
-// }
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
-
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
-  // const config = new DocumentBuilder()
-  //   .setTitle('Marvel')
-  //   .setDescription('Marvel Mock API')
-  //   .setVersion('1.0')
-  //   .addTag('marvel')
-  //   .build();
-
-  // const document = SwaggerModule.createDocument(app, config);
-
-  // SwaggerModule.setup('docs', app, document);
-
-  const PORT = process.env.PORT || 5000;
-  await app.listen(PORT, '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}/api`);
+async function bootstrap(): Promise<Express> {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    app.setGlobalPrefix('api'); // remove if you don't want a global prefix
+    app.enableCors(); // add/adjust if your frontend needs it
+    await app.init();
+    cachedApp = server;
+  }
+  return cachedApp;
 }
-bootstrap();
+
+export default async (
+  req: VercelRequest,
+  res: VercelResponse,
+): Promise<void> => {
+  const app = await bootstrap();
+  app(req as any, res as any);
+};
