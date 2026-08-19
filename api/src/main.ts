27e8af2ce31f47e89d0d18652ef347ee3d588953
Abid Hassan Kiam <kiamhasan267@gmail.com>
2026-08-20 02:47:36 +0600

    refactor(main): remove clustering code and update bootstrap logic for NestJS application

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,46 +1,48 @@
-const cluster = require('cluster');
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import * as os from 'os';
+import express from 'express';
 
-// if (cluster.isPrimary) {
-//   // Use cluster.isPrimary in newer Node.js versions
-//   const numCPUs = os.cpus().length;
-//   console.log(`Primary ${process.pid} is running`);
+const server = express();
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+let isInitialized = false;
 
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`Worker ${worker.process.pid} died`);
-//     cluster.fork(); // Restart the worker when it exits
-//   });
-// } else {
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-// }
+  app.setGlobalPrefix('api');
 
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
-    app.setGlobalPrefix('api');
-    app.useGlobalPipes(new ValidationPipe());
-    const config = new DocumentBuilder()
-      .setTitle('Marvel')
-      .setDescription('Marvel Mock API')
-      .setVersion('1.0')
-      .addTag('marvel')
-      .build();
+  const config = new DocumentBuilder()
+    .setTitle('Marvel')
+    .setDescription('Marvel Mock API')
+    .setVersion('1.0')
+    .addTag('marvel')
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, config);
 
-    SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
-    const PORT = process.env.PORT || 5000;
-    await app.listen(PORT, '0.0.0.0');
-    console.log(`Application is running on: ${await app.getUrl()}/api`);
+  await app.init();
+
+  isInitialized = true;
+}
+
+const bootstrapPromise = bootstrap();
+
+export default async function handler(req: any, res: any) {
+  if (!isInitialized) {
+    await bootstrapPromise;
   }
-bootstrap();
+
+  return server(req, res);
+}
